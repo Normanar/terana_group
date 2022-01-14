052a7934633292cfa17ff0996a8d8fffe37a6cfe
Arman Alimov <527c@mail.ru>
2022-01-14 23:50:21 +0600

    add text in AboutCompany

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {MyGallery} from "../MyGallery/MyGallery";
+import {AboutCompany} from "./AboutCompany";
 
 export function Main () {
     return (
         <>
             <MyGallery/>
-
+            <AboutCompany/>
         </>
     )
 }
