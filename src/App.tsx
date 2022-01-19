d7cd63e2205201adc225d6addb54c37266a1e97d
Arman Alimov <527c@mail.ru>
2022-01-19 21:41:13 +0600

    create Footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Main} from "./components/Main/Main";
+import {Footer} from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         {/*<Slider/>*/}
         {/*<MySlider/>*/}
         <Main/>
+        <Footer/>
     </div>
   );
 }
